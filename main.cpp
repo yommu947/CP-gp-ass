289f28eadce5bea13c534c37b38f765dbaaa3f64
yommu947 <yommu947@gmail.com>
2017-04-13 14:57:50 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,116 +8,112 @@ bool displaypc = false, pcstart = false;
 char chart[10][10];
 
 char alphabet(int num) {//turn number into character
-	switch (num) {
-	case 1: return 'A';
-	case 2: return 'B';
-	case 3: return 'C';
-	case 4: return 'D';
-	case 5: return 'E';
-	case 6: return 'F';
-	case 7: return 'G';
-	case 8: return 'H';
-	case 9: return 'I';
-	case 10: return 'J';
-	}
+    switch (num) {
+        case 1: return 'A';
+        case 2: return 'B';
+        case 3: return 'C';
+        case 4: return 'D';
+        case 5: return 'E';
+        case 6: return 'F';
+        case 7: return 'G';
+        case 8: return 'H';
+        case 9: return 'I';
+        case 10: return 'J';
+        default: return 'A';
+    }
 }
 
 void setting() {
-	int option;
-	cout << "\n\n*** Setting Menu ***\n";
-	cout << "[1] Who starts first\n[2] Display PC's ships\n[3] Sea size\n[4] Number of ships\n[5] Returnto game menu\n";
-	cout << "********************";
-	cout << endl << "Option (1 - 5):";
-	cin >> option;
-	switch (option) {
-	case 1:cout << "Let PC start first?(Input 1 for yes 0 for no)"; cin >> pcstart; setting();
-	case 2:cout << "Display PC's ships?;(Input 1 for yes 0 for no)"; cin >> displaypc; setting();
-	case 3:cout << "Set sea size(5 to 10)"; cin >> seasize; setting();
-	case 4:
-		cout << "Carrier " << carrier << " change to ";
-		cin >> carrier;
-		cout << "Battleship " << battleship << " change to ";
-		cin >> battleship;
-		cout << "Submarine " << submarine << " change to ";
-		cin >> submarine;
-		cout << "Destroyer " << destroyer << " change to ";
-		cin >> destroyer; setting();
-	case 5:break;
-	default:setting();
-	}
-	
+    int option;
+    cout << "\n\n*** Setting Menu ***\n";
+    cout << "[1] Who starts first\n[2] Display PC's ships\n[3] Sea size\n[4] Number of ships\n[5] Returnto game menu\n";
+    cout << "********************";
+    cout << endl << "Option (1 - 5):";
+    cin >> option;
+    switch (option) {
+        case 1:cout << "Let PC start first?(Input 1 for yes 0 for no)"; cin >> pcstart; setting();
+        case 2:cout << "Display PC's ships?;(Input 1 for yes 0 for no)"; cin >> displaypc; setting();
+        case 3:cout << "Set sea size(5 to 10)"; cin >> seasize; setting();
+        case 4:
+            cout << "Carrier " << carrier << " change to ";
+            cin >> carrier;
+            cout << "Battleship " << battleship << " change to ";
+            cin >> battleship;
+            cout << "Submarine " << submarine << " change to ";
+            cin >> submarine;
+            cout << "Destroyer " << destroyer << " change to ";
+            cin >> destroyer; setting();
+        case 5:break;
+        default:setting();
+    }
+    
 }
 
 void sea() {
-	cout << "   ";
-	for (int c = 0; c < seasize; c++) {
-		cout << right << setw(2) << c;
-	}
-	cout << endl << "  +-";
-	for (int c = 0; c < seasize; c++) {
-		cout << "--";
-	}
-	cout << "+";
-
-
-	for (int row = 1; row<= seasize;row++) {
-		cout << endl << alphabet(row) << " | ";
-		for (int col = 1; col <= seasize; col++) {
-			cout << chart[row][col] << " ";
-		}
-		cout << "|";
-
-	}
-	cout << endl << "  +-";
-	for (int c = 0; c < seasize; c++) {
-		cout << "--";
-	}
-	cout << "+";
+    cout << "   ";
+    for (int c = 0; c < seasize; c++) {
+        cout << right << setw(2) << c;
+    }
+    cout << endl << "  +-";
+    for (int c = 0; c < seasize; c++) {
+        cout << "--";
+    }
+    cout << "+";
+    
+    
+    for (int row = 1; row<= seasize;row++) {
+        cout << endl << alphabet(row) << " | ";
+        for (int col = 1; col <= seasize; col++) {
+            cout << chart[row][col] << " ";
+        }
+        cout << "|";
+        
+    }
+    cout << endl << "  +-";
+    for (int c = 0; c < seasize; c++) {
+        cout << "--";
+    }
+    cout << "+";
 }
 
 void clear() {
-	for (int c = 1; c <= 10; c++) {
-		for (int cc = 1; c <= 10; c++) {
-			chart[c][cc] = ' ';
-		}
-	}
+    for (int c = 1; c <= 10; c++) {
+        for (int cc = 1; c <= 10; c++) {
+            chart[c][cc] = ' ';
+        }
+    }
 };
 
 void arrange(){
-	for (int c = 1; c <= carrier; c++) {
-		cout << "Input coordinate of carrier "<<c<<": ";
-	}
-	for (int c = 1; c <= battleship; c++) {}
-	for (int c = 1; c <= submarine; c++) {}
-	for (int c = 1; c <= destroyer; c++) {}
-
+    for (int c = 1; c <= carrier; c++) {
+        cout << "Input coordinate of carrier "<<c<<": ";
+    }
+    for (int c = 1; c <= battleship; c++) {}
+    for (int c = 1; c <= submarine; c++) {}
+    for (int c = 1; c <= destroyer; c++) {}
+    
 }
 
 void game() {
-	sea();
+    sea();
 }
 
-void menu() {
-	int choice;
-	cout << "\nFuck you";//welcome massage
-	cout << "\n\n*** Game Manu ***\n";
-	cout << "[1] Start Game\n[2] Setting\n[3] Instruction\n[4] Credits\n[5] Exit\n";
-	cout << "*****************";
-	cout << endl << "Option (1 - 5):";
-	cin >> choice;
-	switch (choice) {
-	case 1:game(); menu();
-	case 2:setting(); menu();
-	case 3:;
-	case 4:;
-	case 5:;
-	default:menu();
-	}
-
+int main() {
+    int choice;
+    cout << "\nWelcome Captain!";//welcome massage
+    cout << "\n\n*** Game Manu ***\n";
+    cout << "[1] Start Game\n[2] Setting\n[3] Instruction\n[4] Credits\n[5] Exit\n";
+    cout << "*****************";
+    cout << endl << "Option (1 - 5):";
+    cin >> choice;
+    switch (choice) {
+        case 1:game();
+        case 2:setting();
+        case 3:;
+        case 4:;
+        case 5:
+        default: cout << "Please input 1-5";main();
+            
+    }
+    
 }
-
-void main(){
-	
-	menu();
-}
-
