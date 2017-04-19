d6f4ad4a70cbc5bfb8868bebb56df96ee45bcd61
alan04123 <alan04123@gmail.com>
2017-04-19 16:31:27 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ char displaypc = 'n';
 char pcstart = 'p';
 int total_size;
 char chart[10][10];
+char input[4];
 
 char alphabet(int);//turn number into character
 int number(char);//turn number into character
@@ -54,19 +55,20 @@ int main() {
 
 char alphabet(int num) {//turn number into character
 	switch (num) {
-	case 1: return 'A';
-	case 2: return 'B';
-	case 3: return 'C';
-	case 4: return 'D';
-	case 5: return 'E';
-	case 6: return 'F';
-	case 7: return 'G';
-	case 8: return 'H';
-	case 9: return 'I';
-	case 10: return 'J';
+	case 0: return 'A';
+	case 1: return 'B';
+	case 2: return 'C';
+	case 3: return 'D';
+	case 4: return 'E';
+	case 5: return 'F';
+	case 6: return 'G';
+	case 7: return 'H';
+	case 8: return 'I';
+	case 9: return 'J';
 	default: return '*';
 	}
 }
+
 
 int number(char num) {//turn number into character
 	switch (num) {
@@ -90,10 +92,9 @@ int number(char num) {//turn number into character
 	case 'h': return 7;
 	case 'i': return 8;
 	case 'j': return 9;
-	default: return 0;
+	default: return 10;
 	}
 }
-
 void setting() {
 	int option;
 	system("cls");
@@ -104,155 +105,154 @@ void setting() {
 	cin >> option;
 	switch (option) {
 	case 1:	system("cls");
-		cout << "[1] Who starts first" << endl;
-		cout << "Who starts first? ";
-		if (pcstart == 'p') cout << "( Player First NOW)" << endl;
-		else cout << "( Computer First NOW )" << endl;
+			cout << "[1] Who starts first" << endl;
+			cout << "Who starts first? ";
+			if (pcstart == 'p') cout << "( Player First NOW)" << endl;
+			else cout << "( Computer First NOW )" << endl;
 
-		cout << "p: player, c: computer" << endl;
-		cout << "Please type here: ";
-		cin >> pcstart;
-		cout << endl;
-
-		if (pcstart == 'p') cout << "Player First" << endl;
-		else if (pcstart == 'c') cout << "Computer First" << endl;
-		else {
-
-			do {
-			cout << "Please enter p or c only" << endl;
+			cout << "p: player, c: computer" << endl;
 			cout << "Please type here: ";
 			cin >> pcstart;
 			cout << endl;
-		} while (pcstart != 'p' && pcstart != 'c');
 
-		if (pcstart == 'p') cout << "Player First" << endl;
-		else if (pcstart == 'c') cout << "Computer First" << endl;
+			if (pcstart == 'p') cout << "Player First" << endl;
+			else if (pcstart == 'c') cout << "Computer First" << endl;
+			else {
 
-		}
-
-		cout << "State Updated" << endl;
-		system("pause");
-		system("cls");
-		setting();
-		break;
-			
-	case 2: system("cls");
-		cout << "[2] Display PC's ships" << endl;
-		cout << "Display PC's ships? ";
-		if (displaypc == 'y') cout << "(Display PC's ships NOW)" << endl;
-		else cout << "( Not display PC's ships NOW )" << endl;
-
-		cout << "Display = y or Not display = n" << endl;
-		cout << "Please type here: ";
-		cin >> displaypc;
-		cout << endl;
-
-		if (displaypc == 'y') cout << "Display PC's ships" << endl;
-
-		else if (displaypc == 'n') cout << "Not display PC's ships" << endl;
-
-		else {
-			do {
-				cout << "Please enter y or n only" << endl;
+				do {
+				cout << "Please enter p or c only" << endl;
 				cout << "Please type here: ";
-				cin >> displaypc;
+				cin >> pcstart;
 				cout << endl;
-			} while (displaypc != 'y' && displaypc != 'n');
+			} while (pcstart != 'p' && pcstart != 'c');
 
-		if (displaypc == 'y') cout << "Display PC's ships" << endl;
-		else if (displaypc == 'n') cout << "Not display PC's ships" << endl;
+			if (pcstart == 'p') cout << "Player First" << endl;
+			else if (pcstart == 'c') cout << "Computer First" << endl;
 
-		}
+			}
 
-		cout << "state updated" << endl;
-		system("pause");
-		system("cls");
-		setting();
-		break;
+			cout << "State Updated" << endl;
+			system("pause");
+			system("cls");
+			setting();
+			break;
+	case 2: system("cls");
+			cout << "[2] Display PC's ships" << endl;
+			cout << "Display PC's ships? ";
+			if (displaypc == 'y') cout << "(Display PC's ships NOW)" << endl;
+			else cout << "( Not display PC's ships NOW )" << endl;
+
+			cout << "Display = y or Not display = n" << endl;
+			cout << "Please type here: ";
+			cin >> displaypc;
+			cout << endl;
+
+			if (displaypc == 'y') cout << "Display PC's ships" << endl;
+
+			else if (displaypc == 'n') cout << "Not display PC's ships" << endl;
+
+			else {
+				do {
+					cout << "Please enter y or n only" << endl;
+					cout << "Please type here: ";
+					cin >> displaypc;
+					cout << endl;
+				} while (displaypc != 'y' && displaypc != 'n');
+
+			if (displaypc == 'y') cout << "Display PC's ships" << endl;
+			else if (displaypc == 'n') cout << "Not display PC's ships" << endl;
+
+			}
+
+			cout << "state updated" << endl;
+			system("pause");
+			system("cls");
+			setting();
+			break;
 	case 3: system("cls");
-		cout << "[3] Sea size" << endl;
-		cout << "Set sea size (5 to 10) " << "(NOW is " << seasize << " x " << seasize << ")" << endl;
-		cout << "Please type here: ";
-		cin >> seasize;
+			cout << "[3] Sea size" << endl;
+			cout << "Set sea size (5 to 10) " << "(NOW is " << seasize << " x " << seasize << ")" << endl;
+			cout << "Please type here: ";
+			cin >> seasize;
 
-		total_size = carrier * 5 + battleship * 4 + submarine * 3 + destroyer * 2;
+			total_size = carrier * 5 + battleship * 4 + submarine * 3 + destroyer * 2;
 
-		while (seasize * seasize <= total_size) {
-		cout << endl;
-		cout << "Sea size should be from 5 to 10 (each side) and large than total size of ships" << endl;
-		cout << "The total size of ships is " << total_size << endl;
-		cout << "Please type here: ";
-		cin >> seasize;
-		}
+			while (seasize * seasize <= total_size) {
+			cout << endl;
+			cout << "Sea size should be from 5 to 10 (each side) and large than total size of ships" << endl;
+			cout << "The total size of ships is " << total_size << endl;
+			cout << "Please type here: ";
+			cin >> seasize;
+			}
 
-		while (seasize != 5 && seasize != 6 && seasize != 7 && seasize != 8 && seasize != 9 && seasize != 10)
-		{
-		cout << endl;
-		cout << "Sea size should be from 5 to 10 (each side) and large than total size of ships" << endl;
-		cout << "The total size of ships is " << total_size << endl;
-		cout << "Please type here: ";
-		cin >> seasize;
+			while (seasize != 5 && seasize != 6 && seasize != 7 && seasize != 8 && seasize != 9 && seasize != 10)
+			{
+			cout << endl;
+			cout << "Sea size should be from 5 to 10 (each side) and large than total size of ships" << endl;
+			cout << "The total size of ships is " << total_size << endl;
+			cout << "Please type here: ";
+			cin >> seasize;
 			
-		}
+			}
 
-		cout << endl;
-		cout << "Sea size change to " << seasize << " x " << seasize << endl;
-		cout << "state updated" << endl;
-		system("pause");
-		setting();
-		break;
+			cout << endl;
+			cout << "Sea size change to " << seasize << " x " << seasize << endl;
+			cout << "state updated" << endl;
+			system("pause");
+			setting();
+			break;
 
 	case 4:
-		system("cls");
-		cout << "[4] Number of ships" << endl;
+			system("cls");
+			cout << "[4] Number of ships" << endl;
 
-		cout << left << setw(22) << "Type of ship" << "Number" << endl;
-		cout << setw(11) << "carrier" << "(size 5):" << right << setw(5) << carrier << endl;
-		cout << left << setw(11) << "Battleship" << "(size 4):" << right << setw(5) << battleship << endl;
-		cout << left << setw(11) << "Submarine" << "(size 3):" << right << setw(5) << submarine << endl;
-		cout << left << setw(11) << "Destroyer" << "(size 2):" << right << setw(5) << destroyer << endl;
-		cout << endl;
+			cout << left << setw(22) << "Type of ship" << "Number" << endl;
+			cout << setw(11) << "carrier" << "(size 5):" << right << setw(5) << carrier << endl;
+			cout << left << setw(11) << "Battleship" << "(size 4):" << right << setw(5) << battleship << endl;
+			cout << left << setw(11) << "Submarine" << "(size 3):" << right << setw(5) << submarine << endl;
+			cout << left << setw(11) << "Destroyer" << "(size 2):" << right << setw(5) << destroyer << endl;
+			cout << endl;
 
-		cout << "Change the number of ships" << endl;
-		cout << "Carrier" << " change to ";
-		cin >> carrier;
-		cout << "Battleship" << " change to ";
-		cin >> battleship;
-		cout << "Submarine" << " change to ";
-		cin >> submarine;
-		cout << "Destroyer" << " change to ";
-		cin >> destroyer;
+			cout << "Change the number of ships" << endl;
+			cout << "Carrier" << " change to ";
+			cin >> carrier;
+			cout << "Battleship" << " change to ";
+			cin >> battleship;
+			cout << "Submarine" << " change to ";
+			cin >> submarine;
+			cout << "Destroyer" << " change to ";
+			cin >> destroyer;
 
-		total_size = carrier * 5 + battleship * 4 + submarine * 3 + destroyer * 2;
+			total_size = carrier * 5 + battleship * 4 + submarine * 3 + destroyer * 2;
 
-		while ((total_size >= seasize * seasize) || (carrier < 1 || carrier > 5 ) || (battleship < 1 || battleship > 5 ) || (submarine < 1 || submarine > 5 ) || (destroyer < 1 || destroyer > 5 ))
-		{
-		system("cls");
-		cout << endl;
-		cout << "SHIPS SIZE do not exceed the SEA SIZE" << endl;
-		cout << "Please try again " << "( Sea Size = " << seasize << " x " << seasize << " )" << endl;
-		cout << endl;
+			while ((total_size >= seasize * seasize) || (carrier < 1 || carrier > 5 ) || (battleship < 1 || battleship > 5 ) || (submarine < 1 || submarine > 5 ) || (destroyer < 1 || destroyer > 5 ))
+			{
+			system("cls");
+			cout << endl;
+			cout << "SHIPS SIZE do not exceed the SEA SIZE" << endl;
+			cout << "Please try again " << "( Sea Size = " << seasize << " x " << seasize << " )" << endl;
+			cout << endl;
 
-		cout << "Change the number of ships" << endl;
+			cout << "Change the number of ships" << endl;
 
-		cout << "Carrier" << " change to ";
-		cin >> carrier;
-		cout << "Battleship" << " change to ";
-		cin >> battleship;
-		cout << "Submarine" << " change to ";
-		cin >> submarine;
-		cout << "Destroyer" << " change to ";
-		cin >> destroyer;
+			cout << "Carrier" << " change to ";
+			cin >> carrier;
+			cout << "Battleship" << " change to ";
+			cin >> battleship;
+			cout << "Submarine" << " change to ";
+			cin >> submarine;
+			cout << "Destroyer" << " change to ";
+			cin >> destroyer;
 
-		total_size = carrier * 5 + battleship * 4 + submarine * 3 + destroyer * 2;
-		}
+			total_size = carrier * 5 + battleship * 4 + submarine * 3 + destroyer * 2;
+			}
 		
-		cout << endl;
-		cout << "state updated" << endl;
-		system("pause");
-		system("cls");
-		setting();
-		break;
+			cout << endl;
+			cout << "state updated" << endl;
+			system("pause");
+			system("cls");
+			setting();
+			break;
 
 
 
@@ -294,9 +294,10 @@ void sea() {
 	cout << "+"<<endl;
 }
 
+
 void clear() {
-	for (int c = 0; c <= 10; c++) {
-		for (int cc = 0; c <= 10; c++) {
+	for (int c = 1; c <= 10; c++) {
+		for (int cc = 1; c <= 10; c++) {
 			chart[c][cc] = ' ';
 		}
 	}
@@ -305,22 +306,45 @@ void clear() {
 void arrange() {
 	char xchar, ychar;
 	int x, y, direction;
+	bool allow;
 	for (int c = 1; c <= carrier; c++) {
+		do{
+		allow = true;
 		cout << "Input coordinate of carrier "<<c<<"(e.g. C2) :";
-		cin >> ychar >>xchar;
-		y = number(ychar);
-		x = xchar-48;
+		cin >> input;
+		y = number(input[0]);
+		x = input[1] - 48;
 		cout << "\nInput direction of carrier " << c << "(1 for horizontal, 2 for vertical):";
 		cin >> direction;
-		bool allow=true;
+		cout << endl;
+		
+		
 		for (int cc = 0; cc < 5; cc++) {
 			if (direction == 1) {
-				if(chart[y][x + cc] == 'o'){allow=false};
+				if(chart[y][x + cc] == 'o'){allow=false;}
 			}
 			else {
-				if()hart[y + cc][x] == 'o'){allow=false);
+				if(chart[y + cc][x] == 'o'){allow=false;}
 			}
 		}
+		/*
+	if(direction == 1){
+		if(x + 4 > 9){
+			allow=false;
+		}
+	}
+	else{
+		if(y + 4 > 9){
+			allow=false;
+		}
+	}
+	if(y == 10 || x < 0 || x > 9 || input[2] != '\0'|| direction !=1 || direction != 2){
+		allow=false;
+	}
+
+		} while (allow == false);
+		*/
+
 		if(allow=true){
 		for (int cc = 0; cc < 5; cc++) {
 			if (direction == 1) {
@@ -333,13 +357,28 @@ void arrange() {
 		}
 		sea();
 	}
+	
+	
 	for (int c = 1; c <= battleship; c++) {
-		cout << "Input coordinate of battleship " << c << "(e.g. C2) :";
-		cin >> ychar >> xchar;
-		y = number(ychar);
-		x = xchar - 48;
-		cout << "\nInput direction of battleship " << c << "(1 for horizontal, 2 for vertical):";
+	do {
+		allow = true;
+		cout << "Input coordinate of Battleship " << c << "(e.g. C2) :";
+		cin >> input;
+		y = number(input[0]);
+		x = input[1] - 48;
+		cout << "\nInput direction of Battleship " << c << "(1 for horizontal, 2 for vertical):";
 		cin >> direction;
+	for (int cc = 0; cc < 4; cc++) {
+			if (direction == 1) {
+				if(chart[y][x + cc] == 'o'){allow=false;}
+			}
+			else {
+				if(chart[y + cc][x] == 'o'){allow=false;}
+			}
+		}
+		} while (x + 3 > 9 || y + 3 > 9 || allow == false || y == 10 || x < 0 || x > 9 || input[2] != '\0');
+
+		if(allow=true){
 		for (int cc = 0; cc < 4; cc++) {
 			if (direction == 1) {
 				chart[y][x + cc] = 'o';
@@ -348,15 +387,32 @@ void arrange() {
 				chart[y + cc][x] = 'o';
 			}
 		}
+		}
 		sea();
 	}
+
+
+
 	for (int c = 1; c <= submarine; c++) {
+		do{
+		allow = true;
 		cout << "Input coordinate of submarine " << c << "(e.g. C2) :";
-		cin >> ychar >> xchar;
-		y = number(ychar);
-		x = xchar - 48;
+		cin >> input;
+		y = number(input[0]);
+		x = input[1] - 48;
 		cout << "\nInput direction of submarine " << c << "(1 for horizontal, 2 for vertical):";
 		cin >> direction;
+		for (int cc = 0; cc < 3; cc++) {
+			if (direction == 1) {
+				if(chart[y][x + cc] == 'o'){allow=false;}
+			}
+			else {
+				if(chart[y + cc][x] == 'o'){allow=false;}
+			}
+		}
+		} while (x + 2 > 9 || y + 2 > 9 || allow == false || y == 10 || x < 0 || x > 9 || input[2] != '\0');
+
+		if(allow=true){
 		for (int cc = 0; cc < 3; cc++) {
 			if (direction == 1) {
 				chart[y][x + cc] = 'o';
@@ -365,15 +421,51 @@ void arrange() {
 				chart[y + cc][x] = 'o';
 			}
 		}
+		}
 		sea();
 	}
+
+
+
+
 	for (int c = 1; c <= destroyer; c++) {
+	do {
+		allow = true;
 		cout << "Input coordinate of destroyer " << c << "(e.g. C2) :";
-		cin >> ychar >> xchar;
-		y = number(ychar);
-		x = xchar - 48;
+		cin >> input;
+		y = number(input[0]);
+		x = input[1] - 48;
 		cout << "\nInput direction of destroyer " << c << "(1 for horizontal, 2 for vertical):";
 		cin >> direction;
+	for (int cc = 0; cc < 2; cc++) {
+			if (direction == 1) {
+				if(chart[y][x + cc] == 'o'){
+					allow=false;
+				}
+			}
+			else {
+				if(chart[y + cc][x] == 'o'){allow=false;}
+			}
+		}
+	/*
+	if(direction == 1){
+		if(x + 1 > 9){
+			allow=false;
+		}
+	}
+	else{
+		if(y + 1 > 9){
+			allow=false;
+		}
+	}
+	if(y == 10 || x < 0 || x > 9 || input[2] != '\0'|| direction !=1 || direction != 2){
+		allow=false;
+	}
+
+		} while (allow == false);
+	*/
+
+		if(allow=true){
 		for (int cc = 0; cc < 2; cc++) {
 			if (direction == 1) {
 				chart[y][x + cc] = 'o';
@@ -382,14 +474,27 @@ void arrange() {
 				chart[y + cc][x] = 'o';
 			}
 		}
+		}
 		sea();
 	}
+
+
+	
+
+
+
+
+
+
+
 
 }
 
 void game() {
 	sea();
 	arrange();
+
+
 }
 
 
