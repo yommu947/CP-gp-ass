5d513c44a34c2d6a164846c35b34f5b1e8b4ffee
yommu947 <yommu947@gmail.com>
2017-04-20 16:25:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -586,47 +586,66 @@ void arrange() {
 void AIarrange(){
     srand(time(0));
     int AIshipdirection , pt1 , pt2 , errorcheck ;
-    int carrierpt[]
-    for(int i=carrier ; i==0 ; i-- ){
-        AIshipdirection = rand() % 2 + 1;
-            if(AIshipdirection == 1){
-                pt1 = rand() % seasize ;
-                pt2 = rand() % 6 ;
-                AIchart[pt1][pt2] = 'o';
-                AIchart[pt1][pt2 + 1] = 'o';
-                AIchart[pt1][pt2 + 2] = 'o';
-                AIchart[pt1][pt2 + 3] = 'o';
-                AIchart[pt1][pt2 + 4] = 'o';
-                carrierpt[0] = AIshipdirection;
-                carrierpt[1] = pt1;
-                carrierpt[2] = pt2;
-                carrierpt[3] = pt2 + 1;
-                carrierpt[4] = pt2 + 2;
-                carrierpt[5] = pt2 + 3;
-                carrierpt[6] = pt2 + 4;
-                else{
-                    pt1 = rand() % 6 ;
-                    pt2 = rand() % seasize ;
-                    AIchart[pt1][pt2] = 'o';
-                    AIchart[pt1+1][pt2] = 'o';
-                    AIchart[pt1+2][pt2] = 'o';
-                    AIchart[pt1+3][pt2] = 'o';
-                    AIchart[pt1+4][pt2] = 'o';
+    for(int i=1 ; i<=carrier ; i++ ){
+        do {
+            AIshipdirection = rand() % 2 + 1;
+            if (AIshipdirection == 1) {
+                pt1 = rand() % seasize;
+                pt2 = rand() % 6;
+                for (int i = pt2; i < pt2 + 4; i++) {
+                    if (chart[pt1][i] == ' ') {
+                        errorcheck = 1;
+                    } else {
+                        errorcheck = 0;
+                        break;
+                    }
                 }
-
+                if (errorcheck == 1) {
+                    AIchart[pt1][pt2] = 'o';
+                    AIchart[pt1][pt2 + 1] = 'o';
+                    AIchart[pt1][pt2 + 2] = 'o';
+                    AIchart[pt1][pt2 + 3] = 'o';
+                    AIchart[pt1][pt2 + 4] = 'o';
+                    /*carrierpt[0] = AIshipdirection;
+                    carrierpt[1] = pt1;
+                    carrierpt[2] = pt2;
+                    carrierpt[3] = pt2 + 1;
+                    carrierpt[4] = pt2 + 2;
+                    carrierpt[5] = pt2 + 3;
+                    carrierpt[6] = pt2 + 4;*/
+                }
+            } else {
+                pt1 = rand() % 6;
+                pt2 = rand() % seasize;
+                for (int i = pt1; i < pt1 + 4; i++) {
+                    if (chart[i][pt2] == ' ') {
+                        errorcheck = 1;
+                    } else {
+                        errorcheck = 0;
+                        break;
+                    }
+                }
+                if (errorcheck == 1) {
+                    pt1 = rand() % 6;
+                    pt2 = rand() % seasize;
+                    AIchart[pt1][pt2] = 'o';
+                    AIchart[pt1 + 1][pt2] = 'o';
+                    AIchart[pt1 + 2][pt2] = 'o';
+                    AIchart[pt1 + 3][pt2] = 'o';
+                    AIchart[pt1 + 4][pt2] = 'o';
+                }
             }
-
-
-
-
+        }while (errorcheck == 0);
     }
 
 }
 
 void game() {
-    sea();
+    /*sea();*/
     /*arrange();*/
     AIarrange();
+    AIsea();
+
 
 }
 
